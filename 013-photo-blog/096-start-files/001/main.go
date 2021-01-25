837eb9af3cef6aca06f264399b27dcd8a2068bac
ipkalm <ipkalm@outlook.com>
2021-01-25 20:59:21 +0700

    init photoblog

---
+++ 013-photo-blog/096-start-files/001/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var t *template.Template
+
+func init() {
+	t = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", index)
+	log.Fatalln(http.ListenAndServe("127.0.0.1:8080", r))
+}
+
+func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	t.ExecuteTemplate(w, "index.gohtml", nil)
+}
